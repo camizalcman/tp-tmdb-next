a19140f5c705b941bbd5b3c5c988e0bd8a854216
camizalcman <camizalcman@gmail.com>
2026-05-22 19:25:09 -0300

    boton ver mas

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,12 @@
+import Link from "next/link"
+
 const MovieCard = () => {
 
     return (
         <div>
+            <Link href={`/detalle/${id}`}>
+                <button>Ver más</button>
+            </Link>
         </div>
     )
 }
--- src/components/MovieSection.jsx
+++ src/components/MovieSection.jsx
@@ -26,6 +26,7 @@ const MovieSection = () => {
 
     return (
         <div>
+            
         </div>
     )
 }
